40f1c3a164c0ad39117424ea93629dc7557c1545
Godwin Emmanuel <datwhiztekghui@gmail.com>
2025-06-17 14:39:57 +0100

    Remove ignored files from Git tracking

--- sketch-04.js
+++ sketch-04.js
@@ -1,25 +1,44 @@
-import canvasSketch from "canvas-sketch";
-import random from 'canvas-sketch-util/random';
-import mapRange  from 'canvas-sketch-util/math';
-import {Pane} from 'tweakpane';
+const canvasSketch = require("canvas-sketch");
+const random = require("canvas-sketch-util/random");
+const mapRange = require("canvas-sketch-util/math");
+const Tweakpane = require("tweakpane");
 
 const settings = {
   dimensions: [1080, 1080],
   animate: true,
 };
 
-// const params = {
-//   cols: 10,
-//   rows: 10,
-// };
+const params = {
+  cols: 10,
+  rows: 10,
+  bgcolor: "#000000",
+  linecolor: "#ffffff",
+};
+
+const createPane = () => {
+  const pane = new Tweakpane.Pane();
+  let folder;
+
+  folder = pane.addFolder({ title: "Grid", expanded: true });
+  folder.addInput(params, "cols", { min: 2, max: 50, step: 1 });
+  folder.addInput(params, "rows", { min: 2, max: 50, step: 1 });
+  folder.addInput(params, "bgcolor", {
+    label: "Bg Color",
+    type: "color",
+  });
+  folder.addInput(params, "linecolor", {
+    label: "Line Color",
+    type: "color",
+  });
+};
 
 const sketch = () => {
   return ({ context, width, height, frame }) => {
-    context.fillStyle = "black";
+    context.fillStyle = params.bgcolor;
     context.fillRect(0, 0, width, height);
 
-    const cols = 10;
-    const rows = 10;
+    const cols = params.cols;
+    const rows = params.rows;
     const numCells = cols * rows;
 
     const gridw = width * 0.8;
@@ -43,16 +62,16 @@ const sketch = () => {
       const h = cellh * 0.8;
 
       const n = random.noise2D(x + frame * 20, y + frame * 10, 0.001);
-      const angle = n * Math.PI * 0.2; // 0 to 2pi 
+      const angle = n * Math.PI * 0.2; // 0 to 2pi
 
       const scale = (n * 0.5 + 0.5) * 30;
       // const scale = (n + 1) /2 * 30; //another way to scale the line width
-      // const scale = mapRange(n, -1, 1, 0, 30); //another way to scale the line width 
-      // const scale = mapRange(n, -1, 1, 0, 30); //another way to scale the line width 
+      // const scale = mapRange(n, -1, 1, 0, 30); //another way to scale the line width
+      // const scale = mapRange(n, -1, 1, 0, 30); //another way to scale the line width
 
       // lets draw
 
-      context.strokeStyle = "white";
+      context.strokeStyle = params.linecolor;
       context.lineWidth = scale;
 
       context.save();
@@ -60,7 +79,7 @@ const sketch = () => {
       context.translate(margx, margy);
       context.translate(cellw * 0.5, cellh * 0.5);
 
-      // rotate the line by  angle n 
+      // rotate the line by  angle n
       context.rotate(angle);
 
       context.beginPath();
@@ -71,21 +90,10 @@ const sketch = () => {
       context.restore();
     }
   };
-
-  const createPane = () => {
-    const pane = new Pane();
-    let folder;
-
-    folder = pane.addFolder({ title: 'Grid', expanded: true });
-      folder.addInput(params, 'cols', { min: 2, max: 50, step: 1 });
-      folder.addInput(params, 'rows', { min: 2, max: 50, step: 1 });
-  }
 };
 
 createPane();
 canvasSketch(sketch, settings);
-
-
 
 // noise libraries:
 
